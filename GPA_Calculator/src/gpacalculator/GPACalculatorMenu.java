bd4b1d3d977142d40ca8a89ddd608cfdb2372e0f
Liad Hermelin <liad.hermelin@gmail.com>
2019-01-19 14:08:09 -0800

    streamlined GPACalculatorMenu with for loop

--- GPA_Calculator/src/gpacalculator/GPACalculatorMenu.java
+++ GPA_Calculator/src/gpacalculator/GPACalculatorMenu.java
@@ -19,82 +19,13 @@ public class GPACalculatorMenu {
 		while (shouldContinue) {
 			System.out.print("How many classes are you taking this semester? ---> ");
 			int numOfClasses = kboard.nextInt();
-
-			switch (numOfClasses) {
-			case 1:
-				System.out.print("Enter your letter grade in your first class ---> ");
-				str1 = kboard.next();
-				break;
-			case 2:
-				System.out.print("Enter your letter grade in your first class ---> ");
-				str1 = kboard.next();
-				System.out.print("Enter your letter grade in your second class ---> ");
-				str2 = kboard.next();
-				break;
-			case 3:
-				System.out.print("Enter your letter grade in your first class ---> ");
-				str1 = kboard.next();
-				System.out.print("Enter your letter grade in your second class ---> ");
-				str2 = kboard.next();
-				System.out.print("Enter your letter grade in your third class ---> ");
-				str3 = kboard.next();
-				break;
-			case 4:
-				System.out.print("Enter your letter grade in your first class ---> ");
-				str1 = kboard.next();
-				System.out.print("Enter your letter grade in your second class ---> ");
-				str2 = kboard.next();
-				System.out.print("Enter your letter grade in your third class ---> ");
-				str3 = kboard.next();
-				System.out.print("Enter your letter grade in your fourth class ---> ");
-				str4 = kboard.next();
-				break;
-			case 5:
-				System.out.print("Enter your letter grade in your first class ---> ");
-				str1 = kboard.next();
-				System.out.print("Enter your letter grade in your second class ---> ");
-				str2 = kboard.next();
-				System.out.print("Enter your letter grade in your third class ---> ");
-				str3 = kboard.next();
-				System.out.print("Enter your letter grade in your fourth class ---> ");
-				str4 = kboard.next();
-				System.out.print("Enter your letter grade in your fifth class ---> ");
-				str5 = kboard.next();
-				break;
-			case 6:
-				System.out.print("Enter your letter grade in your first class ---> ");
-				str1 = kboard.next();
-				System.out.print("Enter your letter grade in your second class ---> ");
-				str2 = kboard.next();
-				System.out.print("Enter your letter grade in your third class ---> ");
-				str3 = kboard.next();
-				System.out.print("Enter your letter grade in your fourth class ---> ");
-				str4 = kboard.next();
-				System.out.print("Enter your letter grade in your fifth class ---> ");
-				str5 = kboard.next();
-				System.out.print("Enter your letter grade in your sixth class ---> ");
-				str6 = kboard.next();
-				break;
-			case 7:
-				System.out.print("Enter your letter grade in your first class ---> ");
-				str1 = kboard.next();
-				System.out.print("Enter your letter grade in your second class ---> ");
-				str2 = kboard.next();
-				System.out.print("Enter your letter grade in your third class ---> ");
-				str3 = kboard.next();
-				System.out.print("Enter your letter grade in your fourth class ---> ");
-				str4 = kboard.next();
-				System.out.print("Enter your letter grade in your fifth class ---> ");
-				str5 = kboard.next();
-				System.out.print("Enter your letter grade in your sixth class ---> ");
-				str6 = kboard.next();
-				System.out.print("Enter your letter grade in your seventh class ---> ");
-				str7 = kboard.next();
-				break;
+			String[] gradeArray = new String[numOfClasses];
+			
+			for (int i = 0; i<numOfClasses; i++) {
+				System.out.print("Enter letter grade in class " + (i+1) + " ---> ");
+				gradeArray[i] = kboard.next();
 			}
-
-			String[] gradeArray = { str1, str2, str3, str4, str5, str6, str7 };
-
+			
 			GPACalculator calc1 = new GPACalculator();
 			double result = calc1.calculateGPA(gradeArray, numOfClasses);
 			System.out.println("Your GPA this semester is: " + result);
