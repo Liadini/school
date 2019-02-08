48df7ab9ee84969c84a4b06da2e842bcd5a037b2
Liad Hermelin <liad.hermelin@gmail.com>
2019-02-08 12:15:51 -0800

    finished part 3

--- StringToolBox/src/stringtoolbox/StringToolBox.java
+++ StringToolBox/src/stringtoolbox/StringToolBox.java
@@ -12,8 +12,8 @@ package stringtoolbox;
 public class StringToolBox {
 
 	// returns firstname [space] capital LASTNAME
-	public String nameEcho(String s) {
-		return s.substring(0, s.indexOf(" ")) + s.substring(s.indexOf(" ")).toUpperCase();
+	public String nameEcho(String input) {
+		return input.substring(0, input.indexOf(" ")) + input.substring(input.indexOf(" ")).toUpperCase();
 //		int spaceIndex = s.indexOf(" ");
 //		String firstName = s.substring(0, spaceIndex), lastName = s.substring(spaceIndex);
 //		lastName = lastName.toUpperCase();
@@ -21,8 +21,8 @@ public class StringToolBox {
 	}
 
 	// true if specified string ends with a star
-	public boolean endsWithStar(String s) {
-		return (s.length() > 0 && s.endsWith("*"));
+	public boolean endsWithStar(String input) {
+		return (input.length() > 0 && input.endsWith("*"));
 //		if (!s.equals(""))
 //			return s.endsWith("*");
 //		else
@@ -30,8 +30,8 @@ public class StringToolBox {
 	}
 
 	// true if specified string ends with two stars
-	public boolean endsWithTwoStars(String s) {
-		return (s.length() > 1 && s.endsWith("**"));
+	public boolean endsWithTwoStars(String input) {
+		return (input.length() > 1 && input.endsWith("**"));
 //		if (s.length() > 1)
 //			return s.endsWith("**");
 //		else
@@ -39,23 +39,23 @@ public class StringToolBox {
 	}
 
 	// returns last 4 characters of specified string
-	public String last4(String s) {
-		return s.substring(s.length() - 5);
+	public String last4(String input) {
+		return input.substring(input.length() - 5);
 	}
 
 	// returns last 5 characters of specified string
-	public String last5(String s) {
-		return s.substring(s.length() - 6).replace(" ", "");
+	public String last5(String input) {
+		return input.substring(input.length() - 6).replace(" ", "");
 	}
 
 	// returns specified string with first letter moved to the end
-	public String scroll(String s) {
-		return s.substring(1) + s.substring(0, 1);
+	public String scroll(String input) {
+		return input.substring(1) + input.substring(0, 1);
 	}
 
 	// takes lastname, firstname and converts it to firstname lastname
-	public String convertName(String s) {
-		return s.substring(s.indexOf(",") + 1) + " " + s.substring(0, s.indexOf(","));
+	public String convertName(String name) {
+		return name.substring(name.indexOf(",") + 1) + " " + name.substring(0, name.indexOf(","));
 //		int commaIndex = s.indexOf(",");
 //		String lastName = s.substring(0, commaIndex);
 //		String firstName = s.substring(commaIndex + 1);
@@ -63,19 +63,17 @@ public class StringToolBox {
 	}
 
 	// removes all dashes from specified string
-	public String removeDashes(String s) {
-		return s.replace("-", "");
+	public String removeDashes(String input) {
+		return input.replace("-", "");
 	}
 
 	// converts mm/dd/yyyy to dd-mm-yyyy
-	public String dateStr(String s) {
-//		return s.substring(s.indexOf("/") + 1, s.indexOf("/", s.indexOf("/") + 1)) + "-"
-//				+ s.substring(0, s.indexOf("/")) + "-" + s.substring(s.indexOf("/", s.indexOf("/") + 1) + 1); // doesn't work with added zeros
-		int firstSlashIndex = s.indexOf("/");
-		int secondSlashIndex = s.indexOf("/", firstSlashIndex + 1);
-		String mm = s.substring(0, firstSlashIndex);
-		String dd = s.substring(firstSlashIndex + 1, secondSlashIndex);
-		String yyyy = s.substring(secondSlashIndex + 1);
+	public String dateStr(String date) {
+		int firstSlashIndex = date.indexOf("/");
+		int secondSlashIndex = date.indexOf("/", firstSlashIndex + 1);
+		String mm = date.substring(0, firstSlashIndex);
+		String dd = date.substring(firstSlashIndex + 1, secondSlashIndex);
+		String yyyy = date.substring(secondSlashIndex + 1);
 		if (mm.length() < 2)
 			mm = "0" + mm;
 		if (dd.length() < 2)
@@ -84,34 +82,72 @@ public class StringToolBox {
 	}
 
 	// switches 0s and 1s
-	public String negativeBits(String s) {
-		return s.replace("0", "a").replace("1", "0").replace("a", "1").trim();
+	public String negativeBits(String input) {
+		return input.replace("0", "a").replace("1", "0").replace("a", "1").trim();
 //		s = s.replace("0", "a");
 //		s = s.replace("1", "b");
 //		s = s.replace("a", "1");
 //		s = s.replace("b", "0");
 //		return s.trim();
 	}
-	
+
 	// returns true if all characters in specified string are the same
-	public boolean containsSameChar(String s) {
-		return s.length() - s.replaceAll(s.charAt(0) + "", "").length() == s.length();
+	public boolean containsSameChar(String input) {
+		return input.length() - input.replaceAll(input.charAt(0) + "", "").length() == input.length();
 //		String char = s.charAt(0) + "";
 //		int count = s.length() - s.replaceAll(char,"").length();
 //		return count == s.length();
 	}
-	
+
 	// removes any /*...*/ comments from specified string
-	public String removeComments(String s) {
-		if (!s.contains("/*") || !s.contains("*/"))
-			return s;
+	public String removeComments(String input) {
+		if (!input.contains("/*") || !input.contains("*/"))
+			return input;
 		else {
-			return s.replace(s.substring(s.indexOf("/*"), s.indexOf("*/") + 2), "").replace("  ", " ");
+			return input.replace(input.substring(input.indexOf("/*"), input.indexOf("*/") + 2), "").replace("  ", " ");
 //			int firstIndex = s.indexOf("/*");
 //			int lastIndex = s.indexOf("*/");
 //			String remove = s.substring(firstIndex, lastIndex + 2);
 //			return s.replace(remove, "").replace("  ", " ");
 		}
+	}
+
+	/**
+	 * returns the specified string switched with the alphabet, shifted over n units
+	 * 
+	 * @param msg - the message to be encoded
+	 * @param shift - the shift over the alphabet
+	 * @return encoded message
+	 */
+	public String caesar(String msg, int shift) {
+		String result = "";
+	    int len = msg.length();
+	    for(int i = 0; i < len; i++){
+	    	if (Character.isWhitespace(msg.charAt(i))) {
+	    		result += msg.charAt(i);
+	    		continue;
+	    	}
+	        char c = (char)(msg.charAt(i) + shift);
+	        if (c > 'z')
+	            result += (char)(msg.charAt(i) - (26-shift));
+	        else
+	            result += (char)(msg.charAt(i) + shift);
+	    }
+	    return result;
+	}
+
+	// returns true if the reversed string is equal to the specified string
+	public boolean isPalindrome(String input) {
+		return new StringBuilder(input.replace(" ", "")).reverse().toString().equalsIgnoreCase(input.replace(" ", ""));
+	}
+
+	/*
+	 * returns true if the specified string contains no spaces, is longer than 7
+	 * characters, has both lowercase and uppercase characters, and contains numbers
+	 */
+	public boolean validPassword(String pass) {
+		return !pass.contains(" ") && pass.length() > 7 && !pass.toLowerCase().equals(pass) && !pass.toUpperCase().equals(pass)
+				&& pass.matches(".*\\d+.*");
 	}
 
 }
--- StringToolBox/src/stringtoolbox/StringToolBoxTester.java
+++ StringToolBox/src/stringtoolbox/StringToolBoxTester.java
@@ -19,46 +19,63 @@ public class StringToolBoxTester {
 		System.out.println("***Testing: String nameEcho(String s)***");
 		System.out.println("Fred JONES = " + stringTool.nameEcho("Fred Jones"));
 
-		System.out.println("***Testing: boolean endsWithStar(String s)***");
+		System.out.println("\n***Testing: boolean endsWithStar(String s)***");
 		System.out.println("true = " + stringTool.endsWithStar("abcd*"));
 		System.out.println("false = " + stringTool.endsWithStar(""));
 
-		System.out.println("***Testing: boolean endsWithTwoStars(String s)***");
+		System.out.println("\n***Testing: boolean endsWithTwoStars(String s)***");
 		System.out.println("true = " + stringTool.endsWithTwoStars("abcd**"));
 		System.out.println("false = " + stringTool.endsWithTwoStars("*"));
 		System.out.println("false = " + stringTool.endsWithTwoStars(""));
 
-		System.out.println("***Testing: String last4(String s)***");
+		System.out.println("\n***Testing: String last4(String s)***");
 		System.out.println("3456 = " + stringTool.last4("1234 5678 9012 3456"));
 
-		System.out.println("***Testing: String last5(String s)***");
+		System.out.println("\n***Testing: String last5(String s)***");
 		System.out.println("23456 = " + stringTool.last5("1234 5678 9012 3456"));
 
-		System.out.println("***Testing: String scroll(String s)***");
+		System.out.println("\n***Testing: String scroll(String s)***");
 		System.out.println("23451 = " + stringTool.scroll("12345"));
 
-		System.out.println("***Testing: String convertName(String s)***");
+		System.out.println("\n***Testing: String convertName(String s)***");
 		System.out.println("Albert Einstein = " + stringTool.convertName("Einstein, Albert"));
 		
-		System.out.println("***Testing: String removeDashes(String s)***");
+		System.out.println("\n***Testing: String removeDashes(String s)***");
 		System.out.println("987654321 = " + stringTool.removeDashes("987-65-4321"));
 		
-		System.out.println("***Testing: String dateStr(String s)***");
+		System.out.println("\n***Testing: String dateStr(String s)***");
 		System.out.println("31-05-2019 = " + stringTool.dateStr("05/31/2019"));
 		System.out.println("03-05-2019 = " + stringTool.dateStr("5/3/2019"));
 		
-		System.out.println("***Testing: String negativeBits(String s)***");
+		System.out.println("\n***Testing: String negativeBits(String s)***");
 		System.out.println("100001111000 = " + stringTool.negativeBits("011110000111"));
 		System.out.println("01010 = " + stringTool.negativeBits("   10101      "));
 		
-		System.out.println("***Testing: String containsSameChar(String s)***");
+		System.out.println("\n***Testing: String containsSameChar(String s)***");
 		System.out.println("true = " + stringTool.containsSameChar("dddddddd"));
 		System.out.println("false = " + stringTool.containsSameChar("Ccccccc"));
 		System.out.println("false = " + stringTool.containsSameChar("vbnnnnn"));
 		
-		System.out.println("***Testing: String removeComments(String s)***");
+		System.out.println("\n***Testing: String removeComments(String s)***");
 		System.out.println("John was a good man = " + stringTool.removeComments("John was /*not*/ a good man"));
 		System.out.println("John was a good man = " + stringTool.removeComments("John was a good man"));
+		
+		System.out.println("\n***Testing: String caesar(String s)***");
+		System.out.println("Otms bfx f ltti rfs = " + stringTool.caesar("John was a good man", 5));
+		System.out.println("Kpio xbt b hppe nbo = " + stringTool.caesar("John was a good man", 1));
+		
+		System.out.println("\n***Testing: boolean isPalindrome(String s)***");
+		System.out.println("true = " + stringTool.isPalindrome("RaceCar"));
+		System.out.println("true = " + stringTool.isPalindrome("racecar"));
+		System.out.println("false = " + stringTool.isPalindrome("bananas"));
+		System.out.println("true = " + stringTool.isPalindrome("nurses run"));
+		
+		System.out.println("\n***Testing: boolean validPassword(String s)***");
+		System.out.println("true = " + stringTool.validPassword("asuASIUbd1287"));
+		System.out.println("false = " + stringTool.validPassword("asjdhasu328"));
+		System.out.println("false = " + stringTool.validPassword("AASDJASJD0921"));
+		System.out.println("false = " + stringTool.validPassword("asdfjioeJASd"));
+		System.out.println("false = " + stringTool.validPassword("asAS12"));
 	}
 
 }
