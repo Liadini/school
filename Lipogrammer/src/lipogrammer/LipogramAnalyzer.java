0edbfd3c386d3f6c79a24550ec94033e5fdd1013
Liad Hermelin <liad.hermelin@gmail.com>
2019-03-01 12:36:36 -0800

    fixed 60% error

--- Lipogrammer/src/lipogrammer/LipogramAnalyzer.java
+++ Lipogrammer/src/lipogrammer/LipogramAnalyzer.java
@@ -16,43 +16,45 @@ public class LipogramAnalyzer {
 		this.s = s;
 	}
 
+	// returns the string with all specified characters replaced with a pound sign
 	public String mark(char c) {
 		return s.replace(c, '#');
 	}
 
-	public String allWordsWith(char c) {
-		String[] words = s.split("\\s+");
-		String result = "";
-		for (int i = 0; i < words.length; i++) {
-			if (!words[i].contains("" + c))
-				continue;
-			result += words[i] + "\n";
-		}
-		return result;
-	}
-
-//	public String allWordsWith(char letter) {
-//		String answer = "";
-//		int i = this.s.indexOf(letter);
-//		while (i >= 0) {
-//			int start = i;
-//			while ((start >= 0) && ((Character.isLetter(this.s.charAt(start))) || (this.s.charAt(start) == '\'')
-//					|| (this.s.charAt(start) == '-'))) {
-//				start--;
-//			}
-//			int end = i;
-//			while ((end < this.s.length()) && ((Character.isLetter(this.s.charAt(end))) || (this.s.charAt(end) == '\'')
-//					|| (this.s.charAt(end) == '-'))) {
-//				end++;
-//			}
-//			String word = this.s.substring(start + 1, end);
-//			if ((!answer.contains("\n" + word + "\n")) && (!answer.startsWith(word + "\n"))
-//					&& (!answer.endsWith("\n" + word))) {
-//				answer = answer + word + "\n";
-//			}
-//			i = this.s.indexOf(letter, i + 1);
+//	public String allWordsWith(char c) { //DOES NOT WORK WITH TESTER, 60%
+//		String[] words = s.split("\\s+");
+//		String result = "";
+//		for (int i = 0; i < words.length; i++) {
+//			if (!words[i].contains("" + c))
+//				continue;
+//			result += words[i] + "\n";
 //		}
-//		return answer;
+//		return result;
 //	}
+
+	// returns all words with the specified letter, seperated by a new line
+	public String allWordsWith(char c) {
+		String answer = "";
+		int i = s.indexOf(c);
+		while (i >= 0) {
+			int start = i;
+			while ((start >= 0) && ((Character.isLetter(s.charAt(start))) || (s.charAt(start) == '\'')
+					|| (s.charAt(start) == '-'))) {
+				start--;
+			}
+			int end = i;
+			while ((end < s.length())
+					&& ((Character.isLetter(s.charAt(end))) || (s.charAt(end) == '\'') || (s.charAt(end) == '-'))) {
+				end++;
+			}
+			String word = s.substring(start + 1, end);
+			if ((!answer.contains("\n" + word + "\n")) && (!answer.startsWith(word + "\n"))
+					&& (!answer.endsWith("\n" + word))) {
+				answer = answer + word + "\n";
+			}
+			i = s.indexOf(c, i + 1);
+		}
+		return answer;
+	}
 
 }
